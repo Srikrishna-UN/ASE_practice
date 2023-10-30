936df966ead988a8dbda8d0a0f4323ef9a5cc9c9
Srikrishna-UN <119605325+Srikrishna-UN@users.noreply.github.com>
2023-10-30 12:53:49 +0530

    Update bs.py

--- bs.py
+++ bs.py
@@ -18,23 +18,5 @@ def divide(x, y):
         return "Division by zero is not allowed."
     return x / y
 
-if len(sys.argv) != 4:
-    print("Usage: python bs.py <operation> <number1> <number2>")
-    sys.exit(1)
 
-operation = sys.argv[1]
-num1 = float(sys.argv[2])
-num2 = float(sys.argv[3])
-
-if operation == "add":
-    result = add(num1, num2)
-elif operation == "subtract":
-    result = subtract(num1, num2)
-elif operation == "multiply":
-    result = multiply(num1, num2)
-elif operation == "divide":
-    result = divide(num1, num2)
-else:
-    result = "Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'."
-
-print("Result:", result)
+print("Result:", add(18,12))
